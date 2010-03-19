d5b3046a3aef35c9f4bf3c9ac262d139b0e004b6
marcond <marcond@d424a2c0-29f7-00e9-c5cd-8174315b4ec1>
2010-03-19 01:48:02 +0000

    The EducacaoSemDistancia.org Theme first version!

---
+++ theme/esd/topmenu.php
@@ -0,0 +1,9 @@
+<div class="hovermenu clearfix">
+<ul>
+<li class="homemenu"><a href="<?php echo $CFG->wwwroot; ?>">&nbsp;</a></li>
+<li><a href="/">Artigos</a></li>
+<li><a href="/">Forums</a></li>
+<li><a href="/">Contato</a></li>
+</ul>
+</div>
+
