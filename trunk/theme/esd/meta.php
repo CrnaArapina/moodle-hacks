941f82f03a7034ded5b056369f33dfc6163d88b7
marcond <marcond@d424a2c0-29f7-00e9-c5cd-8174315b4ec1>
2010-04-20 15:30:03 +0000

    Changed item hiding from user_style.css to custom.css via meta.php

--- trunk/theme/esd/meta.php
+++ trunk/theme/esd/meta.php
@@ -4,3 +4,13 @@
 <!--[if IE 6]>
     <link rel="stylesheet" type="text/css" href="<?php echo $CFG->httpsthemewww ?>/custom_corners/styles_ie6.css" />
 <![endif]-->
+<?php
+
+    /* Output custom.css optionally */
+    global $CFG;
+    if (file_exists ($CFG->dataroot.'/custom.css')) {
+        echo "<style type='text/css'>\n";
+        readfile ($CFG->dataroot.'/custom.css');
+        echo "</style>\n";
+    }
+
